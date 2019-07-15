967dbd7ba2ddc66edcb8eb8313d7d9c34dd42ab3
bufu2o2 <bufu2o2@gmail.com>
2019-07-15 01:43:28 -0700

    Updates

--- precache-manifest.79728fda07af112d671a85ecfeb6d174.js
+++ precache-manifest.79728fda07af112d671a85ecfeb6d174.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97d40ade369ebfb4eebd577160d97f55",
+    "revision": "8c553c8d0241d11b445a2dc74a3d9c20",
     "url": "/react-port/index.html"
   },
   {
-    "revision": "4bb9de8323544fd3df8e",
-    "url": "/react-port/static/css/main.30ebb5ca.chunk.css"
+    "revision": "4051b268d5ac05291604",
+    "url": "/react-port/static/css/main.562cf16f.chunk.css"
   },
   {
     "revision": "6bea2a1750d5e71fa6f7",
     "url": "/react-port/static/js/2.91120321.chunk.js"
   },
   {
-    "revision": "4bb9de8323544fd3df8e",
+    "revision": "4051b268d5ac05291604",
     "url": "/react-port/static/js/main.5a3e2b8e.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-port/precache-manifest.81c4ca1b3d03bc7dde782a9685687186.js"
+  "/react-port/precache-manifest.79728fda07af112d671a85ecfeb6d174.js"
 );
 
 self.addEventListener('message', (event) => {
