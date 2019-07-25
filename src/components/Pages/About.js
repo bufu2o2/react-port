aab35a9d39c078be38d2aadc63c71f13eecbf2ab
bufu2o2 <bufu2o2@gmail.com>
2019-07-25 00:00:04 -0700

    filled in dummy content for about section for mobile

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,5 +1,6 @@
 import React, {} from 'react';
 import Swiper from '../Swipe/Swiper';
+import mePic from '../../img/chibiIronman.png';
 
 
 
@@ -10,7 +11,19 @@ const About = () => {
         <div>
             <Swiper swipeLeft='Portfolio' swipeRight='Home'/>
             <div className = 'aboutPage'>
-            <h1 className='pageTitle' style={{fontFamily: 'Poiret One, cursive'}}>this.Guy</h1>
+                <h1 className='pageTitle' style={{fontFamily: 'Poiret One, cursive'}}>this.Guy</h1>
+            </div>
+            <div className='container'>
+                <div id='aboutPic'>
+                    <img src={mePic} alt='Iron Man Avatar' />
+                </div>
+                <p id='aboutText'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+                </p>
+                <div id='aboutMoreContainer'>
+                    <i id='aboutMore' />
+                    <div id='aboutMoreLabel'>more</div>
+                </div>
             </div>
         </div>
     )
