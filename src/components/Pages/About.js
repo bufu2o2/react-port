a95e4ca05075140bce735255d7030fbddfae4343
bufu2o2 <bufu2o2@gmail.com>
2019-07-26 09:56:59 -0700

    added minor tweaks to the css of the pic and the placement / opacity of the more container

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -137,7 +137,7 @@ const About = () => {
     return (
         <div>
             <Swiper swipeLeft='Portfolio' swipeRight='Home'/>
-            <div id='tempcontainer'>
+            <div id='aboutContainer'>
                 <h1 className='pageTitle' style={{fontFamily: 'Poiret One, cursive'}}>this.Guy</h1>
                     <div id='aboutPic'><img src={mePic} alt='Iron Man Avatar' /></div>
                     <Text id='aboutText' pose = {display ? 'show' : 'hide' } >
