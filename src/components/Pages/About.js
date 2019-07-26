7a5deb40d438889f5d456384dc5501dfd84eea94
bufu2o2 <bufu2o2@gmail.com>
2019-07-26 03:18:23 -0700

    added a new alignment scheme to the about me section - using grid

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,29 +1,155 @@
-import React, {} from 'react';
+import React, {useState} from 'react';
 import Swiper from '../Swipe/Swiper';
 import mePic from '../../img/chibiIronman.png';
+import posed from 'react-pose';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+
+
+const Text = posed.p({
+    'show': {
+        y: 0,
+        opacity: 1,
+    },
+    'hide': {
+        y: '-100vh',
+        opacity: 0,
+    }
+})
+const Credentials = posed.ul({
+    'show': {
+        y: '-35vh',
+        opacity: 1
+    },
+    'hide': {
+        y: '100vh',
+        opacity: 0
+    }
+})
 
 
 const About = () => {
+    const [display, setdisplay] = useState(true);
+
+    const credArr = [
+        {
+            title: 'UCLA Extension',
+            icon: 'graduation-cap',
+            type: 'fas'
+        },
+        {
+            title: 'George Mason University',
+            icon: 'graduation-cap',
+            type: 'fas'
+        },
+        {
+            title: 'React',
+            icon: 'react',
+            type: 'fab'
+        },
+        {
+            title: 'Javascript',
+            icon: 'js-square',
+            type: 'fab'
+        },
+        {
+            title: 'Mongo Db',
+            icon: 'database',
+            type: 'fas'
+        },
+        {
+            title: 'My Sql',
+            icon: 'database',
+            type: 'fas'
+        },
+        {
+            title: 'Node JS',
+            icon: 'node',
+            type: 'fab'
+        },
+        {
+            title: 'HTML5',
+            icon: 'html5',
+            type: 'fab'
+        },
+        {
+            title: 'CSS3',
+            icon: 'css3-alt',
+            type: 'fab'
+        },
+        {
+            title: 'JQuery',
+            icon: 'js',
+            type: 'fab'
+        },
+        {
+            title: 'Google Firebase',
+            icon: 'gripfire',
+            type: 'fab'
+        },
+        {
+            title: 'Adobe Photoshop',
+            icon: 'image',
+            type: 'fas'
+        },
+        {
+            title: 'Adobe Premiere',
+            icon: 'video',
+            type: 'fas'
+        },
+        {
+            title: 'Adobe After Effects',
+            icon: 'adobe',
+            type: 'fab'
+        },
+        {
+            title: 'Final Cut Pro',
+            icon: 'video',
+            type: 'fas'
+        }
+    ]
+
+  const listItems = credArr.map( (v,i) => (
+            <li key= {i} > 
+                <FontAwesomeIcon id='aboutCredIcon' className='fa' icon={[v.type, v.icon]} />
+                {v.title} 
+            </li>
+    ))
+
+    const contentSwitcher = () => {
+        if(display){
+                return (
+                    <Text id='aboutText' pose={display ? 'show' : 'hide' }>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+                    </Text>
+                )
+        } else {
+                return (
+                    <Credentials id = 'aboutCredentials' pose={display ? 'hide' : 'show' } >
+                    {listItems}
+                    </Credentials>
+                )
+
+        }
+    }
 
 
     return (
         <div>
             <Swiper swipeLeft='Portfolio' swipeRight='Home'/>
-            <div className = 'aboutPage'>
+            <div id='tempcontainer'>
+            {/* <div className = 'aboutPage'> */}
                 <h1 className='pageTitle' style={{fontFamily: 'Poiret One, cursive'}}>this.Guy</h1>
-            </div>
-            <div className='container'>
-                <div id='aboutPic'>
-                    <img src={mePic} alt='Iron Man Avatar' />
-                </div>
-                <p id='aboutText'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
-                </p>
-                <div id='aboutMoreContainer'>
+            {/* </div> */}
+            {/* <div className='aboutContainer'> */}
+                    <div id='aboutPic'><img src={mePic} alt='Iron Man Avatar' /></div>
+            {contentSwitcher()}
+
+                <div id='aboutMoreContainer' onClick={ () => { setdisplay(!display) }} >
                     <i id='aboutMore' />
                     <div id='aboutMoreLabel'>more</div>
                 </div>
+            {/* </div> */}
             </div>
         </div>
     )
