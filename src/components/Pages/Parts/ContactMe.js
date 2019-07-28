0e36ea516754057b55de37b99242f814ea64bbd1
bufu2o2 <bufu2o2@gmail.com>
2019-07-28 14:26:20 -0700

    added direct contact me arrow button with animations

--- src/components/Pages/Parts/ContactMe.js
+++ src/components/Pages/Parts/ContactMe.js
@@ -5,72 +5,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ModalContext} from '../../Contexts/ModalContext';
 
 
-const ContactArrow = posed.div({
-    hoverable: true,
-    pressable: true,
-    init: {
-        y: '0vh',
-        opacity: .2,
-        scale: 1,
-        textShadow: '0 0 0 #000',
-        color: '#33C3F7'
-    },
-    hover: {
-        y: '1vh',
-        opacity: .6,
-        textShadow: '2px 2px 2px #33C3F7',
-        scale: .85
-    },
-    press: {
-        y: '15vh',
-        opacity: 0,
-        textShadow: '2px 2px 2px #33C3F7',
-        scale: .25
-    }
-})
-const ContactWord = posed.div({
-    hoverable: true,
-    pressable: true,
-    init: {
-        y: '0vh',
-        opacity: .1,
-        scale: .5,
-        textShadow: '0 0 0 #000',
-        color: '#33C3F7'
-    },
-    hover: {
-        y: '0vh',
-        opacity: .85,
-        textShadow: '2px 2px 2px #33C3F7',
-        scale: 1
-    },
-    press: {
-        y: '15vh',
-        opacity: 0,
-        textShadow: '2px 2px 2px #33C3F7',
-        scale: .2
-    }
-})
-
 const ContactMe = () => {
 const [misOpen, setMisOpen] = useContext(ModalContext);
-
+const animate = () => {
+    console.log('animation started')
+    document.getElementById('contactMeArrow').className = 'contactMeArrowAnimation';
+    setTimeout(() => {
+        document.getElementById('contactMeArrow').classList.remove('contactMeArrowAnimation');
+    }, 500);
+}
 
 return(
     <Fragment>
-    <ContactArrow id='contactMeArrow' >
+    <div id='contactMeArrow' >
         <FontAwesomeIcon  
         className='fa' 
         icon= {['fas', 'angle-double-down']} 
         onClick= { () => { 
-            console.log('clicked MIS arrows   ', misOpen)
+            animate();
             setMisOpen(!misOpen) 
-            console.log('new mis : ', misOpen)}} 
+        }} 
         />
-    </ContactArrow>
-        {/* <ContactWord id='contactMeWord'>
-        Contact
-        </ContactWord> */}
+    </div>
     </Fragment>
     )
 }
